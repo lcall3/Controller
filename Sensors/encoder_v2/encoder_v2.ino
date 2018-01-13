eeee7f64a72b406142bda62d1ac9ac8f2cfb2725
Muchen He <mansurhe@outlook.com>
2018-01-12 21:49:30 -0800

    Added new version encoder code

---
+++ Sensors/encoder_v2/encoder_v2.ino
@@ -0,0 +1,43 @@
+/* version 2 of encoder test code for PEC12R-4017F-N0024
+ *  
+ */
+
+#define encoderA_pin 2
+#define encoderB_pin 3
+
+volatile int16_t encoderPos = 0;
+uint8_t encoderA_old = 0;
+uint8_t encoderB_new = 0;
+
+bool changed = false;
+
+void encoderA_ISR() {
+    encoderB_new ^ encoderA_old ? encoderPos++ : encoderPos--;
+    encoderA_old = digitalRead(encoderA_pin);
+    changed = true;
+}
+
+void encoderB_ISR() {
+    encoderB_new = digitalRead(encoderB_pin);
+    encoderB_new ^ encoderA_old ? encoderPos++ : encoderPos--;
+    changed = true;
+}
+
+void setup() {
+    pinMode(encoderA_pin, INPUT);
+    pinMode(encoderB_pin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(encoderA_pin), encoderA_ISR, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(encoderB_pin), encoderB_ISR, CHANGE);
+    Serial.begin(115200);
+    Serial.println("RESET");
+}
+
+void loop() {
+    if (changed) {
+        Serial.println(encoderPos, DEC);
+        changed = false;
+    }
+    delay(200);
+    //Serial.println(encoderPos, DEC);
+}
+
