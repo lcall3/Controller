7f310774303d2353e06823a19b6fec92cd495596
Muchen He <mansurhe@outlook.com>
2018-03-03 11:29:27 -0800

    Refactored and reorganized

--- laser/slprj/raccel/laser/__cf_laser.h
+++
@@ -1,3 +0,0 @@
-#ifndef CF_laser_H__
-#define CF_laser_H__
-#endif
--- laser/slprj/raccel/laser/laser_types.h
+++
@@ -1,8 +0,0 @@
-#include "__cf_laser.h"
-#ifndef RTW_HEADER_laser_types_h_
-#define RTW_HEADER_laser_types_h_
-#include "rtwtypes.h"
-#include "builtin_typeid_types.h"
-#include "multiword_types.h"
-typedef struct P_ P ;
-#endif
