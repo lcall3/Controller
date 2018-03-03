256041b2927e35571b1e748db091c7b7446c7a4c
Muchen He <mansurhe@outlook.com>
2018-03-03 10:47:31 -0800

    Encoder FSM written but havent tested

--- Arduino/Examples/encoderFSM/encoderFSM.ino
+++ Arduino/Examples/encoderFSM/encoderFSM.ino
@@ -25,14 +25,48 @@
 #include "PinChangeInterrupt.h"
 
 // Pins
-#define encoderA_pin 5
-#define encoderB_pin 6
+#define encoderA_pin 5      // Corresponds to PORTD 5
+#define encoderB_pin 6      // Corresponds to PORTD 6
+                            // Using 01100000
 
 volatile int encoder_pos;
-volatile bool encoderA;
-volatile bool encoderB;
+volatile byte encoder_state;
+volatile byte encoder_next_state;
+byte encoder_mask;
+
+// Encoder change ISR
+void encoder_ISR() {
+    encoder_next_state = PIND & encoder_mask >> 5;
+    if (
+        (encoder_state == 0x00 & encoder_next_state == 0x01) |
+        (encoder_state == 0x01 & encoder_next_state == 0x03) |
+        (encoder_state == 0x03 & encoder_next_state == 0x02) |
+        (encoder_state == 0x02 & encoder_next_state == 0x00)
+    )
+        encoder_pos++;
+    else if (
+        (encoder_state == 0x00 & encoder_next_state == 0x02) |
+        (encoder_state == 0x02 & encoder_next_state == 0x03) |
+        (encoder_state == 0x03 & encoder_next_state == 0x01) |
+        (encoder_state == 0x01 & encoder_next_state == 0x00)
+    )
+        encoder_pos--;
+}
 
 void setup() {
+    // Reset values
+    encoder_pos = 0;
+    encoder_state = 0;
+
+    // Set encoder mask to 0110 0000 to only read from pin 5 and 6
+    encoder_mask = 1 << (encoderA_pin + 1) | 1 << (encoderB_pin + 1);
+
+    // Set encoder input interrupts
     pinMode(encoderA_pin, INPUT);
     pinMode(encoderB_pin, INPUT);
+    attachPCINT(digitalPinToPCINT(encoderA_pin), encoder_ISR, CHANGE);
+    attachPCINT(digitalPinToPCINT(encoderB_pin), encoder_ISR, CHANGE);
+
+    // Serial
+    Serial.begin(9600);
 }
