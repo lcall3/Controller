002332e90b7b08aaea2834cf3e62a88a76cc540e
Muchen He <mansurhe@outlook.com>
2018-03-15 18:09:22 -0700

    Added global variable def in controller header

--- controller/controller.h
+++ controller/controller.h
@@ -15,8 +15,14 @@
 //
 //##########################################################################
 
-#ifndef CONTROLLER_H
-#define CONTROLLER_H
+#ifndef controller_h
+#define controller_h
+
+// Includes
+#include <Arduino.h>
+
+// Comment out to disable
+#define USE_SERIAL
 
 // ISR functions
 void q0_encoderA_ISR();
@@ -24,6 +30,40 @@ void q0_encoderB_ISR();
 void q1_encoderA_ISR();
 void q1_encoderB_ISR();
 
-// 
+// Global variables
+volatile char vg_q0_encoderA;
+volatile char vg_q0_encoderB;
+volatile char vg_q1_encoderA;
+volatile char vg_q1_encoderB;
+volatile int g_q0_delta_pos;
+volatile int g_q1_delta_pos;
+volatile int g_q0_speed;
+volatile int g_q1_speed;
+
+// Tracked position
+volatile int g_q0_pos;
+volatile int g_q1_pos;
+
+// Accumulated position error
+long g_q0_accum_error;
+long g_q1_accum_error;
+
+// Unsigned counter
+volatile unsigned int counter;
+
+// ISR timed control flags
+#ifdef USE_SERIAL
+volatile char g_output_serial;
+#endif
+volatile char g_control_flag;
+
+// Main state machine
+#define STATE_HOME_Q0_WAIT 0
+#define STATE_HOME_Q0_REPOS 1
+#define STATE_HOME_Q1_WAIT 2
+#define STATE_HOME_Q1_REPOS 3
+#define STATE_RUN 4
+#define STATE_HALT 5
+char g_state;
 
 #endif
