b2e0be57d3c2339cad7901d214b5e492ff9d9c93
Muchen He <mansurhe@outlook.com>
2018-03-23 02:36:00 -0700

    Added socket server

---
+++ sockets_control/public/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+}
+
+function draw() {
+    background(0);
+}
---
+++ sockets_control/server.js
@@ -0,0 +1,8 @@
+
+// Express
+var express = require('express');
+var app = express();
+var server = app.run(8000);
+
+
+console.log("Hello world; the server is alive!");
