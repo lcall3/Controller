0914fa73c0df55ee8a8c8e1fb4fd254e68f21f25
Muchen He <mansurhe@outlook.com>
2018-03-23 03:08:04 -0700

    Emit and receive handler and drawing event handlers

--- sockets_control/public/sketch.js
+++ sockets_control/public/sketch.js
@@ -1,8 +1,45 @@
+
+// Socket client
+var socket;
+
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
+
+    socket = io.connect('http://192.168.0.101:8000');
+
+    // Socket incoming message handling
+    socket.on('mouseDraggedEvent', onMouseDraggedEvent);
+
+    // graphics setup
+    background(0);
+    noStroke();
+}
+
+function mouseDragged() {
+    fill(50, 255, 50);
+    ellipse(mouseX, mouseY, 20, 20);
+    fill(255);
+    ellipse(mouseX, mouseY, 10, 10);
+
+    // Prepare payload to be sent
+    var data = {
+        x: mouseX,
+        y: mouseY
+    };
+
+    // Send load
+    socket.emit('mouseDragged', data);
 }
 
 function draw() {
-    background(0);
+    fill(0, 50);
+    rect(0, 0, width, height);
+}
+
+function onMouseDraggedEvent(data) {
+    fill(255, 50, 50);
+    ellipse(data.x, data.y, 20, 20);
+    fill(255);
+    ellipse(data.x, data.y, 10, 10);
 }
--- sockets_control/server.js
+++ sockets_control/server.js
@@ -2,7 +2,28 @@
 // Express
 var express = require('express');
 var app = express();
-var server = app.run(8000);
+var server = app.listen(8000);
 
-
+app.use(express.static('public'));
 console.log("Hello world; the server is alive!");
+
+// Socket
+var socket = require('socket.io');
+var io = socket(server);
+
+// Even handling
+io.sockets.on('connection', onNewConnection);
+
+
+function onNewConnection(socket) {
+    console.log('new connection: ' + socket.id);
+
+    // Bind rest of the events
+    socket.on('mouseDragged', onMouseDragged);
+
+    function onMouseDragged(data) {
+
+        // Broadcast the data back to all clients
+        socket.broadcast.emit('mouseDraggedEvent', data);
+    }
+}
